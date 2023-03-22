76662dee71838e8302a61f5ad58d5992db00812e
esraa224455 <57463743+esraa224455@users.noreply.github.com>
2023-03-22 19:31:01 +0200

    Update CopyOneListToAnotherSiteWithContent.ps1

--- CopyOneListToAnotherSiteWithContent.ps1
+++ CopyOneListToAnotherSiteWithContent.ps1
@@ -1,19 +1,11 @@
-﻿Add-Type -Path "C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.dll"
-Add-Type -Path "C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.Runtime.dll"
+
 Clear-Host
 $SourceSiteURL = "https://t6syv.sharepoint.com/sites/EsraaTeamSite"
 $TargetSiteURL = "https://t6syv.sharepoint.com/sites/DestinationTeamSite"
-$Cred = Get-PnPStoredCredential -Name "CSOM"
- 
-$Ctx = New-Object Microsoft.SharePoint.Client.ClientContext($SiteUrl)
 
-$Ctx.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($cred.UserName,$cred.Password)
-
-$Dtx = New-Object Microsoft.SharePoint.Client.ClientContext($DestinationSiteURL)
-$Dtx.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($cred.UserName,$cred.Password)
  
 
-$ListName= "AList"
+$ListName= "Projects"
 $TemplateFile ="$env:TEMP\Template.xml"
  
 #Connect to the Source Site
@@ -30,4 +22,3 @@ Connect-PnPOnline -Url $TargetSiteURL -Interactive
  
 #Apply the Template
 Invoke-PnPSiteTemplate -Path $TemplateFile
-
