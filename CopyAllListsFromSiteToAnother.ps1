afaa3baf579ec78876051442a46f511511f5572c
esraa224455 <esraa.elhabash21@gmail.com>
2023-03-26 12:31:25 +0200

    commit

--- CopyAllListsFromSiteToAnother.ps1
+++ CopyAllListsFromSiteToAnother.ps1
@@ -1,25 +1,20 @@
-﻿
-Clear-Host
+﻿Clear-Host
 $SourceSiteURL = "https://t6syv.sharepoint.com/sites/EsraaTeamSite"
 $TargetSiteURL = "https://t6syv.sharepoint.com/sites/DestinationTeamSite"
-
  
-
-
-#$TemplateFile ="$env:TEMP\Template.xml" 
- 
-#Connect to the Source Site
+$number=0
 Connect-PnPOnline -Url $SourceSiteURL -Interactive
 $SourceLists =  Get-PnPList | Where {$_.BaseType -eq "GenericList" -and $_.Hidden -eq $False} | Select Title, Description, ItemCount
 ForEach($SourceList in $SourceLists)
 {
-Connect-PnPOnline -Url $SourceSiteURL -Interactive
-$TemplateFile ="$env:TEMP\Template.xml" 
- 
+
+$number++
 #Connect to the Source Site
+Connect-PnPOnline -Url $SourceSiteURL -Interactive
+$TemplateFile ="$env:TEMP\Template$number.xml" 
 
 $ListName= $SourceList.title
-#Create the Template
+
 Get-PnPSiteTemplate -Out $TemplateFile -ListsToExtract $ListName -Handlers Lists
 
 #Get Data from source List
@@ -29,4 +24,4 @@ Connect-PnPOnline -Url $TargetSiteURL -Interactive
  
 #Apply the Template
 Invoke-PnPSiteTemplate -Path $TemplateFile
- }
+}
